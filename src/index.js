d55d439feb7dcd76ef66fc1812d1b5d08c7727c3
levg34 <guilhem.dross@laposte.net>
2021-01-18 16:40:16 +0100

    add bootstrap

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './style.css'
+import * as bootstrap from 'bootstrap'
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
 document.getElementById('luc').innerHTML = '<a href="google.fr">Test</a>'
+let url = 'http://'+randomWord()+'.fr'
+document.getElementById('luc').innerHTML = '<a href="'+url+'">'+url+'</a>'
+
+function randomWord() {
+    return Math.random().toString(36).replace('0.','')
+}
